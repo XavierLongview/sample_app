b8fef1be33b9f6e33612a6b87678c2414b09feb6
Shane Carroll <scarroll825@gmail.com>
2016-01-06 23:29:57 -0500

    Use SSL and the Puma webserver in production

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title,         "Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title,         "Astrozoica Tourism Buero"
+    assert_equal full_title("Help"), "Help | Astrozoica Tourism Buero"
   end
 end
