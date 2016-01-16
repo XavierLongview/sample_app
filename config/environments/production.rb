40138dd7051d5ef7e623bbbac13c507c0d1df1b2
Shane Carroll <scarroll825@gmail.com>
2016-01-16 02:52:29 -0500

    Fix smtp_setting error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :stmp
   host = 'damp-stream-4113.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_setting = {
+  ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
